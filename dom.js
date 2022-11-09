f6ae531413c10644cc61cebbdfadc8b67673498d
AnsariAhmedFrontEnd <117654588+AnsariAhmedFrontEnd@users.noreply.github.com>
2022-11-09 17:55:38 +0530

    GetElementsByClassName

--- dom.js
+++ dom.js
@@ -16,5 +16,14 @@ header.style.border = 'solid 3px black';
 item.innerHTML = '<b>Add Items</b>';
 item.style.color = 'green';
 
+let listItems = document.getElementsByClassName('list-group-item');
+
+listItems[2].style.backgroundColor = 'green';
+listItems[2].style.color = 'white';
+
+for (let i = 0; i<listItems.length; i++){
+    listItems[i].style.fontWeight = "bold";
+    listItems[i].style.color = 'navy';
+}
 
 
