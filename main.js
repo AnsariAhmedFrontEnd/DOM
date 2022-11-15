dd3f9137db7eb96aa8dda50e5d0ceaedd81f4ece
AnsariAhmedFrontEnd <117654588+AnsariAhmedFrontEnd@users.noreply.github.com>
2022-11-15 18:32:17 +0530

    Scale up the App

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 function store(event) {
     event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const mobile = event.target.number.value;
+    const obj = {
+        name:name,
+        email:email,
+        mobile:mobile
+    };
+
+    let objSeriliazed = JSON.stringify(obj);
+
+    localStorage.setItem(`${email}`,objSeriliazed);
+
+    Object.keys(localStorage).forEach((key) => {
+        detailsOfUsers = localStorage.getItem(key);
+        details = JSON.parse(detailsOfUsers);
+        showUserDetails(details);
+    });
+            
+        
     
-    let inputName = document.getElementById('name').value;
-    
-    let inputEmail = document.getElementById('email').value;
-    
-    localStorage.setItem(inputName, inputEmail);
 }
+
+function showUserDetails(user) {
+    const parentNode = document.getElementById('items');
+
+    const childHTML = `<li>${user.name} - ${user.email}</li>`;
+
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
+}
+
 
