44c64365178cdb96d90d9851b429a768ccc457fe
AnsariAhmedFrontEnd <117654588+AnsariAhmedFrontEnd@users.noreply.github.com>
2022-11-24 22:53:38 +0530

    Booking App Final

--- main.js
+++ main.js
@@ -1,34 +1,70 @@
-function store(event) {
+function save(event) {
     event.preventDefault();
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const mobile = event.target.number.value;
-    const obj = {
-        name:name,
-        email:email,
-        mobile:mobile
-    };
+    let nameInput = event.target.name.value;
+    let emailInput = event.target.email.value;
+    let numberInput = event.target.number.value;
 
-    let objSeriliazed = JSON.stringify(obj);
+    let obj = {
+        name:nameInput,
+        email:emailInput,
+        number: numberInput
+    }
 
-    localStorage.setItem(`${email}`,objSeriliazed);
 
-    Object.keys(localStorage).forEach((key) => {
-        detailsOfUsers = localStorage.getItem(key);
-        details = JSON.parse(detailsOfUsers);
-        showUserDetails(details);
-    });
-            
-        
+    let serilazed = JSON.stringify(obj);
     
+    localStorage.setItem(obj.email, serilazed);
+
+    showUserOnScreen(obj);
 }
 
-function showUserDetails(user) {
+window.addEventListener('DOMContentLoaded', (event) => {
+    Object.keys(localStorage).forEach(key => {
+        const user = JSON.parse(localStorage.getItem(key))
+        showUserOnScreen(user)
+    })
+});
+
+
+function showUserOnScreen(user) {
+    document.getElementById('name').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('number').value = '';
+    if(localStorage.getItem(user.email) != null) {
+        removeUserFromScreen(user.email);
+    }
+
     const parentNode = document.getElementById('items');
-
-    const childHTML = `<li>${user.name} - ${user.email}</li>`;
-
+    const childHTML = `<li id=${user.email}>${user.name} - ${user.email}
+                        <button onclick=deleteUser('${user.email}')>Delete User</button>
+                        <button onclick=edit('${user.name}','${user.email}','${user.number}')>Edit</button>
+                        </li>`;
     parentNode.innerHTML = parentNode.innerHTML + childHTML;
-}
+
+    }
 
 
+    function removeUserFromScreen(email) {
+        const parentNode = document.getElementById('items');
+        const childToBeDeleted = document.getElementById(email);
+        
+        
+        if(childToBeDeleted){
+            parentNode.removeChild(childToBeDeleted);
+        }
+    }
+
+    function deleteUser(email) {
+        localStorage.removeItem(email);
+        removeUserFromScreen(email);
+    }
+
+    function edit(name,email,number) {
+        document.getElementById('name').value = name;
+        document.getElementById('email').value = email;
+        document.getElementById('number').value = number;
+
+        deleteUser(email);
+
+    }
+   
