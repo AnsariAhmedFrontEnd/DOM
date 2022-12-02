b65a3ad6ebbf335bade73ccc0a551b05b7aaa9fa
AnsariAhmedFrontEnd <117654588+AnsariAhmedFrontEnd@users.noreply.github.com>
2022-12-02 19:37:42 +0530

    Function Currying

--- main.js
+++ main.js
@@ -1,70 +1,12 @@
-function save(event) {
-    event.preventDefault();
-    let nameInput = event.target.name.value;
-    let emailInput = event.target.email.value;
-    let numberInput = event.target.number.value;
-
-    let obj = {
-        name:nameInput,
-        email:emailInput,
-        number: numberInput
-    }
-
-
-    let serilazed = JSON.stringify(obj);
-    
-    localStorage.setItem(obj.email, serilazed);
-
-    showUserOnScreen(obj);
+let multiply = function(x) {
+  return function (y) {
+    console.log(x * y);
+  }
 }
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    Object.keys(localStorage).forEach(key => {
-        const user = JSON.parse(localStorage.getItem(key))
-        showUserOnScreen(user)
-    })
-});
 
+let multiplyByTwo = multiply(2);
+multiplyByTwo(3);
 
-function showUserOnScreen(user) {
-    document.getElementById('name').value = '';
-        document.getElementById('email').value = '';
-        document.getElementById('number').value = '';
-    if(localStorage.getItem(user.email) != null) {
-        removeUserFromScreen(user.email);
-    }
-
-    const parentNode = document.getElementById('items');
-    const childHTML = `<li id=${user.email}>${user.name} - ${user.email}
-                        <button onclick=deleteUser('${user.email}')>Delete User</button>
-                        <button onclick=edit('${user.name}','${user.email}','${user.number}')>Edit</button>
-                        </li>`;
-    parentNode.innerHTML = parentNode.innerHTML + childHTML;
-
-    }
-
-
-    function removeUserFromScreen(email) {
-        const parentNode = document.getElementById('items');
-        const childToBeDeleted = document.getElementById(email);
-        
-        
-        if(childToBeDeleted){
-            parentNode.removeChild(childToBeDeleted);
-        }
-    }
-
-    function deleteUser(email) {
-        localStorage.removeItem(email);
-        removeUserFromScreen(email);
-    }
-
-    function edit(name,email,number) {
-        document.getElementById('name').value = name;
-        document.getElementById('email').value = email;
-        document.getElementById('number').value = number;
-
-        deleteUser(email);
-
-    }
-   
+let multiplyByThree = multiply(3);
+multiplyByThree(5);
