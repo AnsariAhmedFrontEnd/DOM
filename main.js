aa75bceddfe0374f73b53f3c1d05717dc97b194e
AnsariAhmedFrontEnd <117654588+AnsariAhmedFrontEnd@users.noreply.github.com>
2023-01-05 18:50:32 +0530

    Axios

--- main.js
+++ main.js
@@ -1,139 +1,249 @@
-// // /**
-// //  * Question 1)
-// //  * 1) We can use this in the global scope.
-// //  * 2) We can use this keyword to point an object in global scope.
-// //  * 3) this inside a method.
-// //  * 4) this inside a function.
-// //  * 5) It is used in call function as an argument.
-// //  * 6) this is also used inside a constructor.
-// //  * 7) this is used to point to the current object of the class.
-// //  * 
-// //  * Question 2)
-// //  * 
-// //  */
+// AXIOS GLOBAL
+axios.defaults.headers.common['X-Auth-Token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
 
-// // // global scope of window object
-// // this.table = 'window table';
-// // console.log(window.table)
+// GET REQUEST
+function getTodos() {
+    // axios({
+    //     method:'get',
+    //     url:'https://jsonplaceholder.typicode.com/todos',
+    //     params: {
+    //         _limit:5
+    //     }
+    // })
+    //     .then(res => showOutput(res))
+    //     .catch(err => console.log(err));
 
+    // axios.get('https://jsonplaceholder.typicode.com/todos',{params:{
+    //     _limit:5
+    // }})
+    // .then(res => showOutput(res))
+    // .catch(err => console.log(err))
 
-// // this.garage = {
-// //    table:'Garage Table'
-// // }
-// // console.log(this.garage.table);
+    axios
+    .get('https://jsonplaceholder.typicode.com/todos?_limit=5',{
+      timeout:5000})
+    .then(res => showOutput(res))
+    .catch(err => console.log(err))
+  }
+  
+  // POST REQUEST
+  function addTodo() {
+    // axios({
+    //     method:'post',
+    //     url:'https://jsonplaceholder.typicode.com/todos',
+    //     data: {
+    //         title: 'New Todo',
+    //         completed: false
+    //     }
+    // })
+    //     .then(res => showOutput(res))
+    //     .catch(err => console.log(err));
 
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title: 'New Todo',
+      completed: false
+    })
+          .then(res => showOutput(res))
+          .catch(err => console.log(err));
+  }
+  
+  // PUT/PATCH REQUEST
+  function updateTodo() {
+    // axios.put('https://jsonplaceholder.typicode.com/todos/1', {
+    //   title: 'New Todo',
+    //   completed: true
+    // })
+    //       .then(res => showOutput(res))
+    //       .catch(err => console.log(err));
 
-// // //this inside an object and inside a method
-// // let johnsRoom = {
-// //    table:'Johns Table',
-// //    CleanTable() {
-// //       console.log(`Cleaning ${this.table}`)
-// //    }
-// // }
+    axios.patch('https://jsonplaceholder.typicode.com/todos/1', {
+      title: 'New Todo',
+      completed: true
+    })
+          .then(res => showOutput(res))
+          .catch(err => console.log(err));
 
-// // console.log(johnsRoom.table);
-// // johnsRoom.CleanTable();
+  }
+  
+  // DELETE REQUEST
+  function removeTodo() {
+    axios.delete('https://jsonplaceholder.typicode.com/todos/1')
+          .then(res => showOutput(res))
+          .catch(err => console.log(err));
+  }
+  
+  // SIMULTANEOUS DATA
+  function getData() {
+    // axios.all([
+    //   axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5'),
+    //   axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+    // ])
+    // .then( res => {
+    //   console.log(res[0]);
+    //   console.log(res[1]);
+    //   showOutput(res[1]);
+    // })
+    // .catch(err => console.log(err));
 
-// // //this inside a function
-// // const cleanTable = function(soap) {
-// //    console.log(`Cleaning ${this.table} using ${soap}`);
-// // }
-
-// // cleanTable.call(this,'Glow')
-// // cleanTable.call(johnsRoom,'MySoap')
-
-
-
-// // //this inside a constructor
-
-// // // let CreatRoom = function(name) {
-// // //    this.table = `${name}s Room`,
-// // // }
-
-// // const jillRoom = new CreatRoom('Jill');
-
-// // //This inside a class
-// // class Room {
-// //    constructor(name) {
-// //       this.table = `${name}s Room`;
-// //    }
-
-// //    cleanTable(soap) {
-// //       console.log(`Cleaning ${this.table} using ${soap}`);
-// //    }
-// // }
-
-// class Student  {
-//     static i = 0;
-//    constructor(studentName,age,phoneNumber,BoardMarks) {
-//       this.studentName = studentName;
-//       this.age = age;
-//       this.phoneNumber = phoneNumber;
-//       this.BoardMarks = BoardMarks; 
-//       Student.i += 1;
-
-//    }
-
-//    isElibleForCollege() {
-//       if(this.BoardMarks > 40) {
-//          console.log(`${this.studentName} is eligible for College`);
-//       }
-//       else{
-//          console.log(`${this.studentName} is eligible NOT for College`);
-//       }
-//    }
-//    static numberOfStudents() {
-//       return Student.i;
-//    }
-// }
-
-// const berlin = new Student('Berlin',40,1234,65);
-// const tokyo = new Student('Tokyo',32,1234,30);
-// const denver = new Student('Denver',28,1234,38);
-// const nairobi = new Student('Nairobi', 30,1234,70);
-// const ansari = new Student('Ansari',26,1234,85);
-
-// berlin.isElibleForCollege();
-// tokyo.isElibleForCollege();
-
-// console.log(Student.numberOfStudents());
-
-
-class Student  {
-      constructor(studentName,age,phoneNumber,BoardMarks) {
-         this.studentName = studentName;
-         this.age = age;
-         this.phoneNumber = phoneNumber;
-         this.BoardMarks = BoardMarks; 
-   
+    axios.all([
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5'),
+      axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+    ])
+    .then(axios.spread((todos,posts) => showOutput(posts)))
+    .catch(err => console.log(err));
+  }
+  
+  // CUSTOM HEADERS
+  function customHeaders() {
+    const config = {
+      headers : {
+        'Content-Type' : 'Application/json',
+        Authorization : 'sometoken'
       }
-   
-      isElibleForPlacement(minMarks) {
-         return (minage) => {
-            if(this.BoardMarks > minMarks && this.age > minage ) {
-               console.log(`${this.studentName} is eligible to sit for Placement`);
-            }
-            else{
-               console.log(`${this.studentName} is eligible NOT to sit for Placement`);
-            }
-         }
+    }
 
-         
-   }
-}
-   
-   let berlin = new Student('Berlin',40,1234,65);
-   let tokyo = new Student('Tokyo',32,1234,30);
-   let denver = new Student('Denver',28,1234,38);
-   let nairobi = new Student('Nairobi', 30,1234,70);
-   let ansari = new Student('Ansari',26,1234,85);
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title: 'New Todo',
+      completed: false
+    },config
+    )
+          .then(res => showOutput(res))
+          .catch(err => console.log(err));
+  }
+  
+  // TRANSFORMING REQUESTS & RESPONSES
+  function transformResponse() {
+    const options = {
+      method:'post',
+      url:'https://jsonplaceholder.typicode.com/todos',
+      data: {
+        title: 'Hello World'
+      },
+      transformResponse: axios.defaults.transformResponse.concat(data => {
+        data.title = data.title.toUpperCase();
+        return data;
+      })
+    }
 
-   berlin.isElibleForPlacement(60)(18) ;
-   tokyo.isElibleForPlacement(60)(18);
-   denver.isElibleForPlacement(60)(18);
-   nairobi.isElibleForPlacement(80)(18);
-   ansari.isElibleForPlacement(80)(18);
+    axios(options).then(res => showOutput(res));
 
-   
+  }
+  
+  // ERROR HANDLING
+  function errorHandling() {
+    axios
+    .get('https://jsonplaceholder.typicode.com/todoss',{
+      ValidityState: function(status) {
+        return status < 500 // Rejects only if status is greate than or equal to 500
+      }
+    })
+    .then(res => showOutput(res))
+    .catch(err => {
+      if(err.response) {
+        //server responded with a status other than 200 range
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
 
+        if(err.response.status === 404){
+          alert('Error: Page Not Found');
+        }
+        else if(err.request) {
+          // Request was made but no response
+          console.log(err.request);
+        }
+        else{
+          console.log(err.message);
+        }
+      }
+    })
+  }
+  
+  // CANCEL TOKEN
+  function cancelToken() {
+    const source = axios.CancelToken.source();
 
+    axios
+    .get('https://jsonplaceholder.typicode.com/todos', {
+      cancelToken:source.token
+    })
+    .then(res => showOutput(res))
+    .catch(thrown => {
+      if(axios.isCancel(thrown)) {
+        console.log('Request Canceled',thrown.message);
+      }
+    });
+
+    if(true) {
+      source.cancel('Request is Canceled');
+    }
+  }
+  
+  // INTERCEPTING REQUESTS & RESPONSES
+  axios.interceptors.request.use(
+    config => {
+      console.log(
+      `${config.method.toUpperCase()} request sent to ${config.url} at ${new Date().getTime()}`);
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  )
+  
+  // AXIOS INSTANCES
+
+  const axiosInstance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+  });
+
+  //axiosInstance.get('/comments').then(res => showOutput(res));
+  
+  // Show output in browser
+  function showOutput(res) {
+    document.getElementById('res').innerHTML = `
+    <div class="card card-body mb-4">
+      <h5>Status: ${res.status}</h5>
+    </div>
+  
+    <div class="card mt-3">
+      <div class="card-header">
+        Headers
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+      </div>
+    </div>
+  
+    <div class="card mt-3">
+      <div class="card-header">
+        Data
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.data, null, 2)}</pre>
+      </div>
+    </div>
+  
+    <div class="card mt-3">
+      <div class="card-header">
+        Config
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.config, null, 2)}</pre>
+      </div>
+    </div>
+  `;
+  }
+  
+  // Event listeners
+  document.getElementById('get').addEventListener('click', getTodos);
+  document.getElementById('post').addEventListener('click', addTodo);
+  document.getElementById('update').addEventListener('click', updateTodo);
+  document.getElementById('delete').addEventListener('click', removeTodo);
+  document.getElementById('sim').addEventListener('click', getData);
+  document.getElementById('headers').addEventListener('click', customHeaders);
+  document
+    .getElementById('transform')
+    .addEventListener('click', transformResponse);
+  document.getElementById('error').addEventListener('click', errorHandling);
+  document.getElementById('cancel').addEventListener('click', cancelToken);
